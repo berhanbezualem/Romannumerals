3ed45c0cdd5f279ed882df4350ded2d53d4b89e7
berhanbezualem <berhanutegebaru@gmail.com>
2020-11-20 21:36:39 -0500

    First Roman numeral commit

---
+++ src/com/romannumerals/RomanNumeral.java
@@ -0,0 +1,31 @@
+package com.romannumerals;
+
+public class RomanNumeral {
+	
+	private final String unit[]= {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+	private final String tens[]= {"","X","XX","XXX","XL","L","LI","LII","LIII","XC"};
+	private final String hundred[]= {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+	private final String thousand[]= {"","M","MM","MMM"};
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=399;
+		RomanNumeral rm=new RomanNumeral();
+		System.out.println("Int number="+i+"   Roman Numeral="+rm.intToRomanNumeral(i));
+
+	}
+	
+	
+	public String intToRomanNumeral(int num)
+	{
+		
+		String roman="";
+		roman=thousand[(num/1000)]+hundred[(num%1000)/100]+tens[((num%1000)%100)/10]+unit[(((num%1000)%100)%10)];
+		
+		return roman;
+				
+				
+	}
+
+}
